f8d238185ff740be25e4de1bd964411a99bcbf0c
Елена <etretjakowa@yandex.ru>
2021-12-14 23:12:04 +0700

    ДЗ ООП

---
+++ src/ru/skypro/Bicycle.java
@@ -0,0 +1,14 @@
+package ru.skypro;
+
+public class Bicycle extends Vehicle {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
+
